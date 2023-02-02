7ba4022c1c3fd77155e392b156984c46ef81c5c4
Kevin_Nyiringango <116556392+Kevin-gram@users.noreply.github.com>
2023-02-02 11:40:46 +0200

    Update 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-<<<<<<< HEAD
-        if ord('a') <= ord(c) <= ord('z'):
-                    return True
-                    else:
-                               return False
-=======
     if ord('a') <= ord(c) <= ord('z'):
         return True
     else:
         return False
->>>>>>> d6ca34be2ce0706dc53a19415f1e9717d0cff122
