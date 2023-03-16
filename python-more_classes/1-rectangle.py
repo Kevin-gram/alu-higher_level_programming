881f72b65a695d17160aa262ce09cadd1a278ca0
kevin-gram <k.nyiringan@alustudent.com>
2023-03-16 02:16:34 -0700

    this is my all default commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Defines Rectangle class."""
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
-    """Representation of a rectangle."""
+    """Represent a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """ new Rectangle.
+        """Initialize a new Rectangle.
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
@@ -15,7 +16,7 @@ class Rectangle:
 
     @property
     def width(self):
-        """ the width of the rectangle."""
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -28,7 +29,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """seting the height of the rectangle."""
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
