0f9272f098e6ff1403720a7236b9f6945e5f0502
Kevin_Nyiringango <116556392+Kevin-gram@users.noreply.github.com>
2023-03-23 04:28:08 -0700

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-'''
+"""
 class square
-'''
+"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-'''
+"""
 Square class
-'''
+"""
 
 
 class Square(Rectangle):
-    ''' Square Class '''
+    """ Square Class """
     def __init__(self, size):
-        ''' instantiation with size '''
+        """ instantiation with size """
         self.integer_validator("size", size)
         self.__size = size
 
