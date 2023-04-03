6fc7f876712eb1553ed74cac01deacf57b0becad
kevin-gram <k.nyiringan@alustudent.com>
2023-04-03 03:31:47 -0700

    this is my all default commit

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends POST to URL
-curl -sX POST -d "email=test@gmail.com&subject=I am okay" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
