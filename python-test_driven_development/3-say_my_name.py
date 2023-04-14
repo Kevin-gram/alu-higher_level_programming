ddb7417b5b498c15ed5e0a9d5002fc060ec2b36f
Kevin_Nyiringango <116556392+Kevin-gram@users.noreply.github.com>
2023-04-14 13:05:43 +0200

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,29 +1,17 @@
->>> say_my_name = __import__('3-say_my_name').say_my_name
+#!/usr/bin/python3
+"""Defines a name function."""
 
 
->>> say_my_name("megema", "jackson")
-My name is umugabo Mugisha
-
-Check wrong value for first name
->>> say_my_name(2005, "megema")
-Traceback (most recent call last):
-        ...
-TypeError: first_name must be a string
-
->>> say_my_name(None, "megema")
-Traceback (most recent call last):
-        ...
-TypeError: first_name must be a string
-
-Check wrong value for last name
->>> say_my_name("muyizeye", 1990)
-Traceback (most recent call last):
-        ...
-TypeError: last_name must be a string
-
-Check missing arguments
->>> say_my_name()
-Traceback (most recent call last):
-	...
-TypeError: say_my_name() missing 1 required positional argument: 'first_name'
- 
+def say_my_name(first_name, last_name=""):
+    """Print name.
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name.
+    Raises:
+        TypeError: If both  of first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
