768786238f87f934ce6b0fc91216ae0fe8d7d379
kevin-gram <k.nyiringan@alustudent.com>
2023-04-16 00:28:26 -0700

    this is my all default commit

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -11,9 +11,9 @@ def print_square(size):
         ValueError: If size is < 0
     """
     if not isinstance(size, int):
-        raise TypeError("size should be  integer")
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError("size must be greater than  0")
+        raise ValueError("size must be >= 0")
 
     for i in range(size):
         [print("#", end="") for j in range(size)]
