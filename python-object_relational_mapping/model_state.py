082dce6f05987bd6212013480f1f5db9f4582da2
kevin-gram <k.nyiringan@alustudent.com>
2023-04-20 06:53:01 -0700

    this is my all default commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-python file that contains the class definition 
-of a State and an instance Base = declarative_base()
+class definition of a State and an instance Base
 """
 
 from sqlalchemy import Column, Integer, String
