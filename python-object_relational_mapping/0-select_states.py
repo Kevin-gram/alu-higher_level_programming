a4c773107ae7864a500f13643112d1333bfec238
Kevin_Nyiringango <116556392+Kevin-gram@users.noreply.github.com>
2023-04-20 12:41:29 +0200

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,12 @@
 
 '''
 a script that lists all states
-from the database
+from My database
 '''
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __Name__ == "__main__":
     conn = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
