3ccdfbba85d493f8e399a79e1bb2e62a5ea067e6
kevin-gram <k.nyiringan@alustudent.com>
2023-06-12 05:22:21 -0700

    this is my all default commit

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const firstNum = argument(process.argv[2]);
+const fun = 'C is fun';
+if (!isNaN(first)) {
+  for (let i = 0; i < firstNum; i++) {
+    console.log(fun);
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
