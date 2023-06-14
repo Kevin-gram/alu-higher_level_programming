f96662dec6ffadbef6bec30115ec11f205ff169f
kevin-gram <k.nyiringan@alustudent.com>
2023-06-14 09:37:06 -0700

    this is my all default commit

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-// A script that display the status code of a GET requestconst url = process.argv[2];
-=======
 // A script that display the status code of a GET request
 
 const url = process.argv[2];
->>>>>>> c4279b0f9d3752da5081829d0369eef225eac971
 const request = require('request');
 request(url, function (error, response, body) {
   if (error) {
