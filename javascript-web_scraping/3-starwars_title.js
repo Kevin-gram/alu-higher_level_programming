6ff1cbf1c440fd43584013da562851786f52b197
kevin-gram <k.nyiringan@alustudent.com>
2023-06-14 09:39:12 -0700

    this is my all default commit

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-// A script that prints the title of a Star Wars movie where the episode num matches a given int
-
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
